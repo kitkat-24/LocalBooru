2c9d068d42b75b1359879cf5544add41fafd0853
William Ruppenthal <williamruppenthal42@gmail.com>
2019-10-03 16:04:51 -0400

    stuff

--- LocalBooru.py
+++ LocalBooru.py
@@ -2,13 +2,38 @@
 # To add an image:
 # python3 LocalBooru.py -a path/to/file.png -artist Bob -character susie -rating safe -series generic_show -tags female clothed cute
 
-import getopt, shutil, sys, uuid, sqlite3
+import getopt
+import pickle
+import shutil
+import sys
+import uuid
 
-conn = sqlite3.connect('localbooru.db')
+from collections import namedtuple
+
 
 # Tags are expected as arguments IFF if the -t flag is given
 short_opt = "hA:Sa:c:r:s:t"
-long_opt = "help add= search artist= character= rating= series= tag".split(" ")
+long_opt = "help add= search artist= character= rating= series= tags".split(" ")
+
+# Info tuple for each image
+Data = namedtuple('Data', 'artist character rating series tags')
+
+
+
+
+def save_index(obj):
+    with open('data/index.pkl', 'wb') as f:
+        pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
+
+def load_index():
+    with open('data/index.pkl', 'rb') as f:
+        return pickle.load(f)
+
+
+try:
+    index = load_index()
+except FileNotFoundError:
+    index = {} # No existing index
 
 def add(inputfile, opts, tags):
     """Add a file to the database.
@@ -22,6 +47,14 @@ def add(inputfile, opts, tags):
     outputfile = str(uuid.uuid4())
     shutil.copy2(inputfile, 'data/' + outputfile)
 
+    obj_data = Data(
+        artist='',
+        character='',
+        rating='',
+        series='',
+        tags=set()
+    )
+
 
 
 def search(opts, tags):
@@ -32,26 +65,36 @@ def search(opts, tags):
 
     :returns: Nothing.
     """
+    results = set()
+    for tag in tags:
+        for image in index:
+            if tag in image.tags:
+                results.add(image)
+
+    return results
+
 
 
 def main(argv):
     try:
         opts, args = getopt.getopt(argv, short_opt, long_opt)
     except getopt.GetoptError:
-        print 'LocalBooru.py -A|-S [filename] -a <artist> -c <character> -r <rating> -s <series> -t <tag1,tag2,...>'
+        print('LocalBooru.py -A|-S [filename] -a <artist> -c <character> -r <rating> -s <series> -t <tag1,tag2,...>')
         sys.exit(2)
+
+    print(str(index))
 
     for opt, arg in opts:
         if opt == '-h':
-            print 'LocalBooru.py -A|-S [filename] -a <artist> -c <character> -r <rating> -s <series> -t <tag1,tag2,...>'
+            print('LocalBooru.py -A|-S [filename] -a <artist> -c <character> -r <rating> -s <series> -t <tag1,tag2,...>')
             sys.exit()
         elif opt in ("-A", "--add"):
             add(arg, opts[1:], args)
         elif opt in ("-S", "--search"):
             search(argv[1:])
 
-    print 'Input file is "', inputfile
-    print 'Output file is "', outputfile
+    print('Input file is {}'.format(inputfile))
+    print('Output file is {}'.format(outputfile))
 
 if __name__ == "__main__":
     main(sys.argv[1:])
