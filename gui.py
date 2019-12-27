90e4979433d53fbbefdee7f5720abc4b99bca2b0
Katerina <kinmersha@gmail.com>
2019-12-27 17:18:37 -0500

    Kind of does something

--- gui.py
+++ gui.py
@@ -1,34 +1,37 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel
 from PyQt5.QtGui import QIcon, QPixmap
+from PyQt5 import QtCore
 
 import LocalBooru as lb
 
 
 class App(QWidget):
-
-    def __init__(self):
+    def __init__(self, qapp):
         super().__init__()
         self.title = 'LocalBooru'
-        self.left = 10
-        self.top = 10
-        self.width = 640
-        self.height = 480
+        self.qapp = qapp
+        #self.left = 10
+        #self.top = 10
+        #self.width = 640
+        #self.height = 480
         self.initUI()
 
     def initUI(self):
         self.setWindowTitle(self.title)
-        self.setGeometry(self.left, self.top, self.width, self.height)
+        #self.setGeometry(self.left, self.top, self.width, self.height)
+        screen = self.qapp.desktop().screenGeometry()
+        width, height = screen.width(), screen.height()
 
         # Create widget
         label = QLabel(self)
         pixmap = QPixmap('image.png')
+        pixmap = pixmap.scaled(width, height, QtCore.Qt.KeepAspectRatio)
         label.setPixmap(pixmap)
-        self.resize(pixmap.width(),pixmap.height())
-
         self.showMaximized()
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = App()
+    ex = App(app)
     sys.exit(app.exec_())
