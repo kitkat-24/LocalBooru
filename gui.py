f1e327fdda0c93997ba2b6504973ca274c640d80
Katerina <kinmersha@gmail.com>
2019-12-27 23:46:01 -0500

    Working on adding main fields

--- gui.py
+++ gui.py
@@ -1,18 +1,16 @@
 import sys
 from collections import namedtuple
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QApplication, QMainWindow, QAction, QWidget, QFrame, QLabel, QVBoxLayout, QHBoxLayout, QGridLayout, QPushButton, QMessageBox, QLineEdit
+from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QIcon, QPixmap
 from PyQt5 import QtCore
 
+import ImgButton as ib
 import LocalBooru as lb
 
-Dimension = namedtuple('Dimension', 'w h')
-thumbnail = Dimension(100, 100)
+thumbnail_size = QtCore.QSize(100, 100)
 icon_path = './basic-ui-icons/SVGs/'
-menu_icon_size = QtCore.QSize(48, 48)
-menu_button_size = QtCore.QSize(menu_icon_size.width() + 8, menu_icon_size.height() + 8)
-print(menu_button_size)
+menu_icon_size = QtCore.QSize(64, 64)
 
 
 class LBmain(QWidget):
@@ -40,49 +38,83 @@ class LBmain(QWidget):
         self.mainLayout.addWidget(self.topbarFrame)
         self.topbarLayout = QHBoxLayout(self.topbarFrame)
         self.addTopbarButtons()
-        self.topbarFrame.show()
+        #self.topbarFrame.show()
 
+        # Create layout to hold main screen elements: lefthand tag column and
+        # central image gallery/close-up view.
+        self.centralFrame = QFrame(self.mainFrame)
+        self.centralLayout = QHBoxLayout(self.centralFrame)
 
+        # Create left column widget + layout:
+        self.leftFrame = QFrame(self.centralFrame)
+        self.centralLayout.addWidget(self.leftFrame)
+        self.leftLayout = QVBoxLayout(self.leftFrame)
+        # Populate column
+        self.addLeftCol()
+        #self.leftFrame.show()
 
-
-        #mainseg = QHBoxLayout()
-        #leftcol = QVBoxLayout()
-        #browser = QGridLayout(self)
+        # Create image display grid widget + layout:
+        self.imFrame = QFrame(self.centralFrame)
+        self.centralLayout.addWidget(self.imFrame)
+        self.imLayout = QGridLayout(self.imFrame)
+        #self.imFrame.show()
 
         # Done
         #self.showMaximized()
-        self.show()
+        self.centralFrame.show()
+        #self.show()
 
     def addTopbarButtons(self):
         """Populate the buttons in the topbar."""
-        add_but = QPushButton('', self.topbarFrame)
+        pixmap = QPixmap(icon_path + 'Plus.svg')
+        pixmap = pixmap.scaled(
+                menu_icon_size.width(),
+                menu_icon_size.height(),
+                transformMode=QtCore.Qt.SmoothTransformation)
+        add_but = ib.ImgButton(pixmap, self.topbarFrame)
         add_but.clicked.connect(self.add_dialogue)
-        add_but.setIcon(QIcon(icon_path + 'Plus.svg'))
-        add_but.setIconSize(menu_icon_size)
+        #add_but.setSize(menu_icon_size)
 
-        share_but = QPushButton('', self.topbarFrame)
+        pixmap = QPixmap(icon_path + 'Share.svg')
+        pixmap = pixmap.scaled(
+                menu_icon_size.width(),
+                menu_icon_size.height(),
+                transformMode=QtCore.Qt.SmoothTransformation)
+        share_but = ib.ImgButton(pixmap, self.topbarFrame)
         share_but.clicked.connect(self.share_dialogue)
-        share_but.setIcon(QIcon(icon_path + 'Share.svg'))
-        share_but.setIconSize(menu_icon_size)
+        #share_but.setSize(menu_icon_size)
 
-        delete_but = QPushButton('', self.topbarFrame)
+        pixmap = QPixmap(icon_path + 'Trash.svg')
+        pixmap = pixmap.scaled(
+                menu_icon_size.width(),
+                menu_icon_size.height(),
+                transformMode=QtCore.Qt.SmoothTransformation)
+        delete_but = ib.ImgButton(pixmap, self.topbarFrame)
         delete_but.clicked.connect(self.delete_dialogue)
-        delete_but.setIcon(QIcon(icon_path + 'Trash.svg'))
-        delete_but.setIconSize(menu_icon_size)
+        #delete_but.setSize(menu_icon_size)
 
         self.search_query = QLineEdit()
         search_but = QPushButton('Search', self.topbarFrame)
         search_but.clicked.connect(self.search)
 
         # Add to topbar
-        self.topbarLayout.addWidget(add_but, QtWidgets.QLayout.SetMinimumSize)
-        self.topbarLayout.addWidget(share_but, QtWidgets.QLayout.SetMinimumSize)
-        self.topbarLayout.addWidget(delete_but, QtWidgets.QLayout.SetMinimumSize)
-        self.topbarLayout.addWidget(self.search_query, QtWidgets.QGridLayout.SetMaximumSize)
-        self.topbarLayout.addWidget(search_but, QtWidgets.QLayout.SetMinimumSize)
+        self.topbarLayout.addWidget(add_but, QLayout.SetMinimumSize)
+        self.topbarLayout.addWidget(share_but, QLayout.SetMinimumSize)
+        self.topbarLayout.addWidget(delete_but, QLayout.SetMinimumSize)
+        self.topbarLayout.addWidget(self.search_query, QGridLayout.SetMaximumSize)
+        self.topbarLayout.addWidget(search_but, QLayout.SetMinimumSize)
 
         # Stretch to fit
         self.topbarLayout.addStretch()
+
+    def addLeftCol(self):
+        """Populate the left column with tags."""
+        for i in range(7):
+            widget = QWidget(self.leftFrame)
+            label = QLabel(widget)
+            label.setText(f'bs tag {i}')
+            widget.show()
+            self.leftLayout.addWidget(widget, QLayout.SetMinimumSize)
 
 
     #---------------------------------------------------------------------------
@@ -115,5 +147,6 @@ class LBmain(QWidget):
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
+    app.setAttribute(QtCore.Qt.AA_UseHighDpiPixmaps)
     lbm = LBmain(app)
     sys.exit(app.exec_())
